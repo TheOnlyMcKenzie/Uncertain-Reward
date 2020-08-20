810b7e7cefefef3f020ba2c9aa25ab2d09ff7b0f
TheOnlyMcKenzie <69824772+TheOnlyMcKenzie@users.noreply.github.com>
2020-08-20 12:32:17 +1200

    Add files via upload

---
+++ ASSESSMENT_CODE.py
@@ -0,0 +1,22 @@
+# Uncertain_Reward.py
+# Created by: James McKenzie
+# Created on: 20/08/2020
+# Last modified on: 20/08/2020
+
+''' Version: 1.0
+    In this version I have set up the basic skeleton of the program
+    Each component can be worked on separately or simultaneously
+    This will act as the master copy of the code, updated once each component is completed
+'''
+
+BASE_REWARD = # Number
+score = 0
+
+def gamble():
+    # Gambling code goes here
+
+def problems():
+    # Lists/dictionaries of the problems go here
+
+def main():
+    # Overall routine goes here
